786a57055ba88c027dc2686c02ee919465bca910
Raioni <87760199+Raioni@users.noreply.github.com>
2021-09-13 21:12:11 -0300

    Update 6.sh

--- Lista 3/6.sh
+++ Lista 3/6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Digite uma senha: " pwd
+read -sp "Digite uma senha: " pwd
 echo ''
 if [ $(echo $pwd | grep -E ".{8,}") ] && [ $(echo $pwd | grep -E "[A-Z]") ] && [ $(echo $pwd | grep -E "[0-9]") ];
 then
